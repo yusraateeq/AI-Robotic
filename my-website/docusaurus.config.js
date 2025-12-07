1c9225a691bd0ba8f3450b08e454674629466460
Yusra Ateeq <yusraateeq112@gmail.com>
2025-12-07 12:57:37 +0500

    commit

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -95,7 +95,6 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
@@ -135,10 +134,7 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
+              
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
